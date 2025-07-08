56f9e22eb6ae96c742eac7be7ff6a012d1d99476
Zehs <andrey5764zehsteam@gmail.com>
2025-07-08 14:17:27 -0500

    Updated 404 page.

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import SEO from '$lib/components/SEO.svelte';
 	import Header from '$lib/components/ui/Header.svelte';
+	import Main from '$lib/components/ui/Main.svelte';
 </script>
 
 <SEO
@@ -9,31 +10,11 @@
 
 <Header />
 
-<main>
+<Main>
 	<br />
 	<h2>Error 404. This page does not exist.</h2>
-</main>
+</Main>
 
 <style>
-	main {
-		margin: 0 20em;
-	}
-
-	@media only screen and (max-width: 1420px) {
-		main {
-			margin: 0 15em;
-		}
-	}
-
-	@media only screen and (max-width: 1250px) {
-		main {
-			margin: 0 10em;
-		}
-	}
-
-	@media only screen and (max-width: 960px) {
-		main {
-			margin: 0 2em;
-		}
-	}
+    
 </style>
