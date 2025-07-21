6c3964d17634dfc21edd40aef2ff2d100ac8e754
Zehs <andrey5764zehsteam@gmail.com>
2025-07-20 21:51:59 -0500

    Enhanced the page layout by introducing new information sections for enemy and event pricing, including visual elements for Twitch Bits and subs. Updated styling for better clarity and responsiveness, and replaced SVG icons with a Star component for consistency.

--- src/routes/page/+page.svelte
+++ src/routes/page/+page.svelte
@@ -6,6 +6,8 @@
 	import Header from '../Header.svelte';
 	import PageInfo from './PageInfo.svelte';
 	import CardList from './CardList.svelte';
+	import bitsImage from '$lib/assets/bits.png';
+	import Star from '../Star.svelte';
 
 	const pageState = getPageState();
 
@@ -57,34 +59,45 @@
 		/>
 
 		{#if pageState.enemies.length || pageState.events.length}
-			<div class="usage-info">
+			<div class="info-section">
 				<p>
 					To spawn enemies or trigger events, simply cheer the specified amount of bits in the
 					streamer's chat.
 				</p>
 				<p>
-					If multiple enemies share the same prices, or multiple events, one will be chosen at random from
-					that group.
+					If multiple enemies share the same prices, or multiple events, one will be chosen at
+					random from that group.
 				</p>
 			</div>
-            <div class="usage-info">
-                <h2>Important Information</h2>
-                <p>
-                    Enemy prices can't match event prices. While duplicate prices are allowed within enemies or within events, no enemy can have the same price as any event.
-                </p>
-                <p>
-                    RANDOM ENEMY and RANDOM EVENT cannot share the same price.
-                </p>
-                <p>
-                    Please notify the streamer if you notice any prices like this.
-                </p>
-            </div>
+			<div class="info-section">
+				<h2>Important Information</h2>
+				<p>Only the host of the lobby can spawn enemies and trigger events!</p>
+				<p>You can edit the prices in the in-game MODS menu.</p>
+				<p>
+					Enemy prices can't match event prices. While duplicate prices are allowed within enemies
+					or within events, no enemy can have the same price as any event.
+				</p>
+				<p>RANDOM ENEMY and RANDOM EVENT cannot share the same price.</p>
+				<p>Please notify the streamer if you notice any prices like this.</p>
+				<div class="currencies-container">
+					<div class="currency-container">
+						<img src={bitsImage} alt="Twitch Bits" />
+						<p>Twitch bits</p>
+					</div>
+					<div class="currency-container">
+						<Star />
+						<p>Twitch subs</p>
+					</div>
+				</div>
+			</div>
 		{/if}
 
 		{#if pageState.enemies.length && pageState.subEnemySpawnCountMultipliers}
-			<div class="sub-info">
+			<div class="info-section">
 				<h2>Sub Tier Multipliers</h2>
-                <p>Using subs to spawn enemies will multiply the enemy spawn count by the tier of the sub.</p>
+				<p>
+					Using subs to spawn enemies will multiply the enemy spawn count by the tier of the sub.
+				</p>
 				<p>
 					Tier 1: Multiplies enemy spawn count by {pageState.subEnemySpawnCountMultipliers.tier1}
 				</p>
@@ -119,12 +132,29 @@
 		margin: 0 20em;
 	}
 
-	.usage-info {
+	.info-section {
 		margin-bottom: 1em;
 	}
 
-	.sub-info {
-		margin-bottom: 1em;
+	.currencies-container {
+		display: flex;
+		gap: 0.75em;
+	}
+
+	.currency-container {
+		display: flex;
+		align-items: center;
+	}
+
+	.currency-container img {
+		width: 15px;
+		height: 15px;
+		margin-right: 4px;
+	}
+
+	.currency-container:not(:last-child)::after {
+		content: '/';
+		margin-left: 0.75em;
 	}
 
 	@media only screen and (max-width: 1420px) {
--- src/routes/page/Card.svelte
+++ src/routes/page/Card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import bitsImage from '$lib/assets/bits.png';
+	import Star from '../Star.svelte';
 	import type { CardData } from './PageState.svelte';
 
 	let {
@@ -28,13 +29,7 @@
 		<p class="price">{card.price}</p>
 		{#if card.subPrice}
 			<p class="price-separator">/</p>
-			<svg viewBox="0 0 20 20" aria-hidden="true"
-				><path
-					fill-rule="evenodd"
-					d="M11.456 8.255 10 5.125l-1.456 3.13-3.49.485 2.552 2.516-.616 3.485L10 13.064l3.01 1.677-.616-3.485 2.553-2.516-3.491-.485zM7.19 6.424l-4.2.583c-.932.13-1.318 1.209-.664 1.853l3.128 3.083-.755 4.272c-.163.92.876 1.603 1.722 1.132L10 15.354l3.579 1.993c.846.47 1.885-.212 1.722-1.132l-.755-4.272 3.128-3.083c.654-.644.268-1.723-.664-1.853l-4.2-.583-1.754-3.77c-.406-.872-1.706-.872-2.112 0L7.19 6.424z"
-					clip-rule="evenodd"
-				></path></svg
-			>
+			<Star />
 			<p class="price">{card.subPrice}</p>
 		{/if}
 	</div>
@@ -93,15 +88,7 @@
 		margin-right: 4px;
 	}
 
-	.price-container svg {
-		width: 17px;
-		height: 17px;
-		margin-right: 4px;
-		fill: var(--purple-light);
-	}
-
-	.price-container img,
-	.price-container svg {
+	.price-container img {
 		transform: translateY(-2px);
 	}
 
